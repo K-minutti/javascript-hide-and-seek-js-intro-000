e17a877621481356d1f2f9889f961f2d4f5a1530
Kevin Minutti <minutti70@gmail.com>
2020-03-24 19:52:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,14 +8,14 @@ function nestedTarget() {
   return document.getElementById('nested').querySelector('div.target')
 
 }
+
 function increaseRankBy(n) {
-  const upRank = document.getElementById('app').querySelectorAll('ul.ranked-list')
-
-  for (let i = 0; i < upRank.length; i++){
-    upRank[i].innerHTML = ((parseInt(upRank[i].innerHTML)) + n)
-    }
-
+  var ranks = document.querySelectorAll('ul.ranked-list li');
+  for (var i = 0; i < ranks.length; i++) {
+    ranks[i].innerHTML = parseInt(ranks[i].innerHTML) + n
+  }
 }
+
 
 function deepestChild() {
   var deep = document.getElementById('grand-node').querySelectorAll('div')
